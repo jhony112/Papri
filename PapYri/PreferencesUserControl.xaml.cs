b431c97b21950b3eb72ad4b548688b37e93d64ad
jhony112 <shipaclick@gmail.com>
2015-03-20 03:30:28 -0700

    Papri Win 8.1 app

---
+++ PapYri/PreferencesUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PapYri
+{
+    public sealed partial class PreferencesUserControl : UserControl
+    {
+        public PreferencesUserControl()
+        {
+            this.InitializeComponent();
+            if (ApplicationData.Current.RoamingSettings.Values.ContainsKey("Remember"))
+                Remember.IsOn = (bool)ApplicationData.Current.RoamingSettings.Values["Remember"];
+        }
+
+        private void OnToggled(object sender, RoutedEventArgs e)
+        {
+            ApplicationData.Current.RoamingSettings.Values["Remember"] = Remember.IsOn;
+        }
+    }
+}
